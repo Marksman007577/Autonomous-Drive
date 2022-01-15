8929593f1f709472871950a247eadb42f5839e5a
Mark <omefemark@gmail.com>
2022-01-15 21:12:19 +0100

    updated ACC plant model and re tested the Cruise control with modified plant model

--- ACC/ACC_Script.m
+++ ACC/ACC_Script.m
@@ -7,9 +7,9 @@ V0 = 27.80;              % Initial velocity of the car in m/s
 Vw_0 = 1.2;             % Initial wind velocity in m/s
 A = 2.24;               % Cross sectional Area of the tyre surface
 rho = 1.2;              % pressure in Km/m^3
-
+sim_time = 50;          % simulation time in seconds
 
 %% Plant Model
 plant_num = 9.2251*1e-4;
-plant_den = [1 1/50.531];
+plant_den = [1 0.0466];
 G_plant = tf(plant_num, plant_den);
