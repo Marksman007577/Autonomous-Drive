77bbab95794acce4e1c4edb6dd0eeb6c263d8e50
Mark <omefemark@gmail.com>
2022-02-26 23:25:01 +0100

    Lateral motion using Stanley Control
    Full working function for lateral control using Stanley Controller

---
+++ Stanley control/stanley_controller.m
@@ -0,0 +1,32 @@
+%% Stanley Controller 
+clc;
+% clear all;
+close all;
+%% Load the scenerio data file generated from Driving Scenerio Designer App
+%scenerio = scenerio1;                    %This represents function from Drive scenerio lane change.
+%scenerio = scenerio2;                    %This represents function from Drive scenerio fig eight course.
+scenerio = scenerio3;                     %This represents function from Drive scenerio city road
+n = size(scenerio);
+
+for ii = 1:n(2)
+    refPose = [scenerio(ii).ActorPoses.Position(1);scenerio(ii).ActorPoses.Position(2);scenerio(ii).ActorPoses.Yaw];
+end
+xRef = refPose(1,:);
+yRef = refPose(2,:);
+yawRef = refPose(3,:);
+Vref = 30;
+refPose = refPose'; % Reference way points stored in a matrix form
+
+%% Define reference times for plotting
+% sim_time = 120;   % Simulation time for scenerio2
+% sim_time = 100;   % Simulation time for scenerio1
+sim_time = 150;     % Simulation time for scenerio3
+s = size(xRef);
+tRef = (linspace(0,sim_time, s(2))); % Time variable used for 2D visualization block for plotting reference points
+
+%% Defining parameters used in the model
+L = 4.7;  %Wheelbase length
+K = 5;    %K is the tuning
+X_0 = xRef(1,1); % Initial vehicle posiiton
+Y_0 = yRef(1,1); % Initial vehicle posiiton
+psi_0 = 0; % Yaw angle for lane change scenerio 
